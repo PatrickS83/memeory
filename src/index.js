ff95f1056e44c381c319d889308a8150746abb00
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-16 12:00:24 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
