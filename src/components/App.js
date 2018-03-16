b2c1a23230815e8f6e820cde7078a01059d4e031
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-16 12:36:39 +0100

    add Layout component and navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import Layout from './Layout';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <section className="hero is-primary">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                Primary title
+        <Layout>
+          <section className="hero is-primary">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Primary title
               </h1>
-              <h2 className="subtitle">
-                Primary subtitle
+                <h2 className="subtitle">
+                  Primary subtitle
               </h2>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </Layout>
       </React.Fragment>
     );
   }
