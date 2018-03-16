9dbb174c46f4e61c768104b3e71bc00af3c35d48
PatrickSchuelke <p-schuelke@gmx.de>
2018-03-16 18:26:55 +0100

    added heading

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -17,46 +17,49 @@ class Settings extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className='columns'>
-          <div className="field column is-two-thirds">
-            <label className="label">Choose a theme</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                name="theme"
-                placeholder="e.g Cats"
-                onChange={e => this.props.handleSettingsInputChange(e)}
-                value={this.props.theme}
-              />
+      <section className="section is-medium">
+        <h1 className="title has-text-centered">Choose your settings</h1>
+        <div className="container">
+          <div className='columns'>
+            <div className="field column is-two-thirds">
+              <label className="label">Choose a theme</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  name="theme"
+                  placeholder="e.g Cats"
+                  onChange={e => this.props.handleSettingsInputChange(e)}
+                  value={this.props.theme}
+                />
+              </div>
+              <p className="help">This chooses the gif theme for the next game</p>
             </div>
-            <p className="help">This chooses the gif theme for the next game</p>
-          </div>
 
-          <div className="field column">
-            <label className="label">How many cards?</label>
-            <div className="control">
-              <input
-                className="input"
-                name="cards"
-                type="number"
-                max="16"
-                min="4"
-                step="2"
-                onChange={e => this.props.handleSettingsInputChange(e)}
-                value={this.props.cards}
-              />
+            <div className="field column">
+              <label className="label">How many cards?</label>
+              <div className="control">
+                <input
+                  className="input"
+                  name="cards"
+                  type="number"
+                  max="16"
+                  min="4"
+                  step="2"
+                  onChange={e => this.props.handleSettingsInputChange(e)}
+                  value={this.props.cards}
+                />
+              </div>
+              <p className="help">This chooses the number of cards for the next game (max 16)</p>
             </div>
-            <p className="help">This chooses the number of cards for the next game (max 16)</p>
           </div>
-        </div>
-        <a
-          className="button is-primary playbutton"
-          onClick={this.handleStartGameClick}>
-          Start the game!
+          <a
+            className="button is-primary playbutton"
+            onClick={this.handleStartGameClick}>
+            Start the game!
         </a>
-      </div>
+        </div>
+      </section>
     );
   }
 }
