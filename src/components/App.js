f0d302b19330ae1da8476c5e8c907d5c842d9455
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-09 14:55:00 +0200

    minor refactoring and prettier reformat

--- src/components/App.js
+++ src/components/App.js
@@ -40,18 +40,19 @@ class App extends Component {
     const searchTheme = this.state.settings.theme;
     const APIKEY = 'PPGBBI41SF35';
 
-    fetch(`https://api.tenor.com/v1/search?tag=${searchTheme}&key=${APIKEY}&media_filter=minimal`)
+    fetch(
+      `https://api.tenor.com/v1/search?tag=${searchTheme}&key=${APIKEY}&media_filter=minimal`
+    )
       .then(response => response.json())
-      .then((data) => {
-        const cards = data.results.splice(0, amountGifs).map((gif, i) => (
-          {
-            id: i,
-            url: gif.media[0].gif.url,
-            clicked: 0,
-            matched: false
-          }));
+      .then(data => {
+        const cards = data.results.splice(0, amountGifs).map((gif, i) => ({
+          id: i,
+          url: gif.media[0].gif.url,
+          clicked: 0,
+          matched: false
+        }));
         // preloading images, so that user doesn't have to wait when a card is clicked
-        cards.forEach((x) => {
+        cards.forEach(x => {
           const preloadImage = new Image();
           preloadImage.src = x.url;
         });
@@ -59,13 +60,13 @@ class App extends Component {
       });
   };
 
-  handleCardClick = (url) => {
+  handleCardClick = url => {
     const cards = [...this.state.cards];
     cards.find(card => card.url === url).clicked += 1;
     this.setState({ cards });
 
     let { activeCards } = this.state;
-    this.setState({ activeCards: activeCards += 1 });
+    this.setState({ activeCards: (activeCards += 1) });
     if (activeCards === 2) this.checkCardMatch();
   };
 
@@ -74,25 +75,26 @@ class App extends Component {
     this.fetchGifs();
   };
 
-  handleSettingsInputChange = (e) => {
+  handleSettingsInputChange = e => {
     const elem = e.currentTarget;
     const settings = { ...this.state.settings };
-    settings[elem.name] = elem.name === 'theme' ? elem.value : Number(elem.value);
+    settings[elem.name] =
+      elem.name === 'theme' ? elem.value : Number(elem.value);
     this.setState({ settings });
   };
 
   checkCardMatch = () => {
-    // deep copy of array of objects. [... state] is only shallow copy. is there better way?
+    // deep copy of array of objects. [... state] is only shallow copy.
+    // TODO: find better way
     const cards = JSON.parse(JSON.stringify(this.state.cards));
     if (cards.find(card => card.clicked === 2)) {
       console.log('match!');
       cards.find(card => card.clicked === 2).matched = true;
     } else console.log('try again');
-    cards.forEach(card => card.clicked = 0);
+    cards.forEach(card => (card.clicked = 0));
     // show cards for some time before resetting
     setTimeout(() => {
-      this.setState({ cards });
-      this.setState({ activeCards: 0 });
+      this.setState({ cards, activeCards: 0 });
       this.checkForWin();
     }, 2500);
   };
@@ -103,8 +105,7 @@ class App extends Component {
   };
 
   resetGame = () => {
-    this.setState({ won: false });
-    this.setState({ cards: [] });
+    this.setState({ won: false, cards: [] });
   };
 
   render() {
@@ -112,8 +113,7 @@ class App extends Component {
       <React.Fragment>
         <Layout>
           <Header />
-          {this.state.cards.length
-            ?
+          {this.state.cards.length ? (
             <Gamecontainer
               size={this.state.size}
               cardData={this.state.cards}
@@ -122,13 +122,14 @@ class App extends Component {
               hasWon={this.state.won}
               resetGame={this.resetGame}
             />
-            :
+          ) : (
             <Settings
               handleSettingsInputChange={this.handleSettingsInputChange}
               startGame={this.startGame}
               theme={this.state.settings.theme}
               cards={this.state.settings.cards}
-            />}
+            />
+          )}
         </Layout>
       </React.Fragment>
     );
