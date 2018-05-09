d28289aa9d50fbf75951dad39baf62b655d04dc7
Patrick Schuelke <p-schuelke@gmx.de>
2018-05-09 14:55:16 +0200

    add tests for resetGame function

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,8 +3,31 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-  it('renders the <App/> component without crashing', () => {
+  it('renders without crashing', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.length).toEqual(1);
+  });
+
+  describe('function resetGame()', () => {
+    it('resets the game settings in state', () => {
+      const wrapper = shallow(<App />);
+      const instance = wrapper.instance();
+      instance.resetGame();
+      expect(wrapper.state('won')).toBe(false);
+      expect(wrapper.state('cards').length).toBe(0);
+    });
+
+    it('doesnt change any other than "won" or "cards" settings in state', () => {
+      const wrapper = shallow(<App />);
+      const instance = wrapper.instance();
+      const stateCopy = { ...wrapper.state() };
+      instance.resetGame();
+      const filteredKeys = Object.keys(stateCopy).filter(
+        x => x !== 'won' && x !== 'cards'
+      );
+      filteredKeys.forEach(key => {
+        expect(stateCopy[key]).toEqual(wrapper.state(key));
+      });
+    });
   });
 });
