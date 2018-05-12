ccbc66ac2e15950059ef53a9eee3f45021da9560
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-12 12:33:31 +0200

    refactored settings component into stateless function

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,59 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Settings extends Component {
-  static propTypes = {
-    handleSettingsInputChange: PropTypes.func,
-    startGame: PropTypes.func,
-    cards: PropTypes.number,
-    theme: PropTypes.string
-  };
-
-  render() {
-    return (
-      <section className="section is-medium">
-        <h1 className="title has-text-centered">Choose your settings</h1>
-        <div className="container">
-          <div className="columns">
-            <div className="field column is-two-thirds">
-              <label className="label">Choose a theme</label>
-              <div className="control">
-                <input
-                  className="input"
-                  type="text"
-                  name="theme"
-                  placeholder="e.g Cats"
-                  onChange={e => this.props.handleSettingsInputChange(e)}
-                  value={this.props.theme}
-                />
-              </div>
-              <p className="help">This chooses the gif theme for the next game</p>
-            </div>
-
-            <div className="field column">
-              <label className="label">How many cards?</label>
-              <div className="control">
-                <input
-                  className="input"
-                  name="cards"
-                  type="number"
-                  max="16"
-                  min="4"
-                  step="2"
-                  onChange={e => this.props.handleSettingsInputChange(e)}
-                  value={this.props.cards}
-                />
-              </div>
-              <p className="help">This chooses the number of cards for the next game (max 16)</p>
-            </div>
+const Settings = props => (
+  <section className="section is-medium">
+    <h1 className="title has-text-centered">Choose your settings</h1>
+    <div className="container">
+      <div className="columns">
+        <div className="field column is-two-thirds">
+          <label htmlFor="themeInput" className="label">
+            Choose a theme
+          </label>
+          <div className="control">
+            <input
+              id="themeInput"
+              className="input"
+              type="text"
+              name="theme"
+              placeholder="e.g Cats"
+              onChange={props.handleSettingsInputChange}
+              value={props.theme}
+            />
           </div>
-          <a className="button is-primary playbutton" onClick={this.props.startGame}>
-            Start the game!
-          </a>
+          <p className="help">This chooses the gif theme for the next game</p>
         </div>
-      </section>
-    );
-  }
-}
+
+        <div className="field column">
+          <label htmlFor="cardsInput" className="label">
+            How many cards?
+          </label>
+          <div className="control">
+            <input
+              id="cardsInput"
+              className="input"
+              name="cards"
+              type="number"
+              max="16"
+              min="4"
+              step="2"
+              onChange={props.handleSettingsInputChange}
+              value={props.cards}
+            />
+          </div>
+          <p className="help">This chooses the number of cards for the next game (max 16)</p>
+        </div>
+      </div>
+      <button className="button is-primary playbutton" onClick={props.startGame}>
+        Start the game!
+      </button>
+    </div>
+  </section>
+);
+
+Settings.propTypes = {
+  handleSettingsInputChange: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+  cards: PropTypes.number.isRequired,
+  theme: PropTypes.string.isRequired
+};
 
 export default Settings;
