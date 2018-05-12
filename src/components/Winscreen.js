25d0d3c4ddf8fa1bfc135928710a3dcaa1282ed8
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-12 13:57:53 +0200

    refactored some a11y problems

--- src/components/Winscreen.js
+++ src/components/Winscreen.js
@@ -3,31 +3,27 @@ import PropTypes from 'prop-types';
 
 const Winscreen = props => (
   <React.Fragment>
-    <h1 className="title has-text-centered">
-      YOU WON !!!!
-      </h1>
+    <h1 className="title has-text-centered">YOU WON !!!!</h1>
     <div className="columns is-multiline is-mobile">
       <div className="column is-6">
         <figure className="image is-square">
-          <img src="https://media.giphy.com/media/gsrkIo59dnI8E/giphy.gif" />
+          <img src="https://media.giphy.com/media/gsrkIo59dnI8E/giphy.gif" alt="" />
         </figure>
       </div>
       <div className="column is-6">
         <figure className="image is-square">
-          <img src="https://media.giphy.com/media/T6l1LBKmtUu9a/giphy.gif" />
+          <img src="https://media.giphy.com/media/T6l1LBKmtUu9a/giphy.gif" alt="" />
         </figure>
       </div>
-      <a
-        className="button is-info playbutton"
-        onClick={props.resetGame}>
+      <button className="button is-info playbutton" onClick={props.resetGame}>
         Play again!
-      </a>
+      </button>
     </div>
   </React.Fragment>
 );
 
 Winscreen.propTypes = {
-  resetGame: PropTypes.func
+  resetGame: PropTypes.func.isRequired
 };
 
 export default Winscreen;
