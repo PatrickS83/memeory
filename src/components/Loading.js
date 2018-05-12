7bed03930ee62544d7e18dd0df233e4d1133fe71
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-12 19:43:37 +0200

    add loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => (
+  <section className="section is-medium has-text-centered">
+    <div className="sk-folding-cube">
+      <div className="sk-cube1 sk-cube" />
+      <div className="sk-cube2 sk-cube" />
+      <div className="sk-cube4 sk-cube" />
+      <div className="sk-cube3 sk-cube" />
+    </div>
+    <h2>Packaging cards ...</h2>
+  </section>
+);
+
+export default Loading;
