cb21adc40ad83f979511aad73722ca1cae0c64bc
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-13 12:33:20 +0200

    decreased footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className="footer" style={{ padding: '2.5rem 1.5rem 4rem' }}>
     <div className="container">
       <div className="content has-text-centered">
         <p>
           <strong>Memeory</strong> by <a href="#">Patrick Schülke</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.{' '}
+          <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.{' '}
           <a href="#">Impressum</a>.
         </p>
       </div>
