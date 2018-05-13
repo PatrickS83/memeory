5929ae56e0ee0adf15fa3d784a41868b0ea1eef5
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-13 12:01:15 +0200

    added landing page animation

---
+++ src/components/LandingPageAnimation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPageAnimation = () => (
+  <div id="splash">
+    <div className="anim">
+      <div id="loader">
+        <img src="./img/memeory-logo-white.png" id="landingLogo" alt="Landingpage Logo" />
+      </div>
+    </div>
+  </div>
+);
+
+export default LandingPageAnimation;
