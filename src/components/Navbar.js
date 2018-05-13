8223c463ca205a16a0566a15c2c63d6632f0f8e6
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-13 12:01:29 +0200

    add logo and github link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,18 @@ import React from 'react';
 const Navbar = () => (
   <nav className="navbar is-primary">
     <div className="navbar-brand">
-      <a className="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+      <a className="navbar-item" href="https://patrickschuelke.me/memeory/">
+        <img src="./img/memeory-logo-white.png" alt="Website Logo" width="150" height="auto" />
       </a>
     </div>
 
     <div className="navbar-menu">
-      <div className="navbar-start">
-        <a className="navbar-item" href="#">
-          Home
-        </a>
-      </div>
-
       <div className="navbar-end">
         <div className="navbar-item">
           <div className="field is-grouped">
             <p className="control">
-              <a className="button is-primary" href="#">
-                <span>Download</span>
+              <a className="button is-primary" href="https://github.com/PatrickS83/">
+                <span>Github</span>
               </a>
             </p>
           </div>
